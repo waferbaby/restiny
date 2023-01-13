996dbd0cc0bc70a84746970d9733a3a07de501e9
Daniel Bogan <d@waferbaby.com>
2023-01-13 03:30:52 +0000

    Add the default_headers method, rename to search_users

--- lib/restiny/client.rb
+++ lib/restiny/client.rb
@@ -5,7 +5,7 @@ require 'faraday/follow_redirects'
 
 module Restiny
   class Client
-    API_BASE_URL = "https://www.bungie.net/Platform/Destiny2"
+    API_BASE_URL = "https://www.bungie.net/Platform"
 
     attr_accessor :api_key
 
@@ -15,8 +15,20 @@ module Restiny
 
     # Account methods
 
-    def search_players(name, platform = PLATFORM_ALL)
-      get("/SearchDestinyPlayer/#{platform}/#{name}")
+    def search_users(name, page = 0)
+      response = post("/User/Search/GlobalName/#{page}", { displayNamePrefix: name })
+      search_results = response.body&.dig('Response', 'searchResults')
+
+      return [] if search_results.empty?
+
+      search_results.map do |user|
+        Restiny::User.new(
+          display_name: user['bungieGlobalDisplayName'],
+          display_name_code: user['bungieGlobalDisplayNameCode'],
+          membership_id: user['bungieNetMembershipId'],
+          memberships: user['destinyMemberships']
+        )
+      end
     end
 
     def get(endpoint_url, params = {})
@@ -29,13 +41,15 @@ module Restiny
 
     private
 
+    def default_headers
+      {
+        'User-Agent': "restiny v#{Restiny::VERSION}",
+        'X-API-KEY': @api_key
+      }
+    end
+
     def connection
-      @connection ||= Faraday.new(
-        headers: {
-          'Content-Type': 'application/json',
-          'X-API-KEY': @api_key
-        }
-      ) do |faraday|
+      @connection ||= Faraday.new(headers: default_headers) do |faraday|
         faraday.request :json
         faraday.request :url_encoded
         faraday.response :json
