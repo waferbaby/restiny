1819e47ca8c9efc1eef156d675450e6462a7dee1
Daniel Bogan <d@waferbaby.com>
2023-01-13 01:24:26 +0000

    Add the player search method, and enable a bunch of Faraday middleware

--- lib/restiny/client.rb
+++ lib/restiny/client.rb
@@ -1,13 +1,22 @@
 # frozen_string_literal: true
 
+require 'faraday'
+require 'faraday/follow_redirects'
+
 module Restiny
   class Client
-    API_BASE_URL = "https://www.bungie.net/Platform/Destiny2/"
+    API_BASE_URL = "https://www.bungie.net/Platform/Destiny2"
 
     attr_accessor :api_key
 
     def initialize(api_key)
       @api_key = api_key
+    end
+
+    # Account methods
+
+    def search_players(name, platform = PLATFORM_ALL)
+      get("/SearchDestinyPlayer/#{platform}/#{name}")
     end
 
     def get(endpoint_url, params = {})
@@ -26,7 +35,13 @@ module Restiny
           'Content-Type': 'application/json',
           'X-API-KEY': @api_key
         }
-      )
+      ) do |faraday|
+        faraday.request :json
+        faraday.request :url_encoded
+        faraday.response :json
+        faraday.response :follow_redirects
+        faraday.response :raise_error
+      end
     end
   end
 end
