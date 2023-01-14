de6e61f750e10f55b255779a979606bed8ea21b9
Daniel Bogan <d@waferbaby.com>
2023-01-14 00:12:48 +0000

    Switch to the platform constants

--- lib/restiny/membership.rb
+++ lib/restiny/membership.rb
@@ -1,20 +1,16 @@
 # frozen_string_literal: true
 
 module Restiny
-  MEMBERSHIP_XBOX = 1
-  MEMBERSHIP_PLAYSTATION = 2
-  MEMBERSHIP_STEAM = 3
-
   class Membership
     attr_reader :id, :type, :cross_save_override, :icon_path, :is_public, :types
 
     def self.platform(type)
       case type
-      when MEMBERSHIP_XBOX
+      when PLATFORM_XBOX
         :xbox
-      when MEMBERSHIP_PLAYSTATION
+      when PLATFORM_PSN
         :playstation
-      when MEMBERSHIP_STEAM
+      when PLATFORM_STEAM
         :steam
       end
     end
