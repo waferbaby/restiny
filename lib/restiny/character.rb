2ead5769489ebf6c0a2808b5ac846484618fa235
Daniel Bogan <d@waferbaby.com>
2023-02-06 02:29:24 +0000

    Add the initial Character class

---
+++ lib/restiny/character.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Restiny
+  class Character
+    attr_accessor :id, :playtime, :light_level, :stats, :emblem, :progression
+
+    def initialize(id:, session_playtime:, total_playtime:, light_level:, stats:, emblem:, progression:)
+      @id = id
+      @playtime = { session: session_playtime, total: total_playtime }
+      @light_level = light_level
+      @stats = stats
+      @emblem = emblem
+      @progression = progression
+  end
+end
+
