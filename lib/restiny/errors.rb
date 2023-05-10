64105ff8bfb78bebd61ee95e176f30834165783c
Daniel Bogan <d@waferbaby.com>
2023-05-10 08:54:26 +0000

    Add more specific error subclasses

--- lib/restiny/errors.rb
+++ lib/restiny/errors.rb
@@ -2,4 +2,18 @@
 
 module Restiny
   class Error < StandardError; end
+  class RequestError < Error; end
+    class InvalidParamsError < RequestError; end
+    class RateLimitedError < RequestError
+    end
+
+  class ResponseError < Error; end
+  class NetworkError < Error
+    attr_accessor :status_code
+
+    def initialize(message, status_code = nil)
+      @status_code = status_code
+      super(message)
+    end
+  end
 end
