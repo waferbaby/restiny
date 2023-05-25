9fc9745333bb32314ae21b02e670ccc8dc569c00
Daniel Bogan <d@waferbaby.com>
2023-05-25 08:43:20 +0000

    Rework the constants

--- lib/restiny/constants.rb
+++ lib/restiny/constants.rb
@@ -1,10 +1,39 @@
 # frozen_string_literal: true
 
-PLATFORM_ALL = -1
-PLATFORM_XBOX = 1
-PLATFORM_PSN = 2
-PLATFORM_STEAM = 3
-PLATFORM_EPIC = 6
+module Restiny
+  module Platform
+    ALL = -1
+    XBOX = 1
+    PSN = 2
+    STEAM = 3
+    EPIC = 6
+  end
+
+  module ComponentType
+    PROFILES = "Profiles"
+    PROFILE_INVENTORIES = "ProfileInventories"
+    CHARACTERS = "Characters"
+    CHARACTER_INVENTORIES = "CharacterInventories"
+  end
+
+  module ItemLocation
+    UNKNOWN = 0
+    INVENTORY = 1
+    VAULT = 2
+    VENDOR = 3
+    POSTMASTER = 4
+  end
+
+  module TierType
+    UNKNOWN = 0
+    CURRENCY = 1
+    BASIC = 2
+    COMMON = 3
+    RARE = 4
+    SUPERIOR = 5
+    EXOTIC = 6
+  end
+end
 
 COMPONENT_TYPE_PROFILES = "Profiles"
 COMPONENT_TYPE_PROFILE_INVENTORIES = "ProfileInventories"
