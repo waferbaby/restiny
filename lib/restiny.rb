9096aa551f115906201a96847a246c658df1005b
Daniel Bogan <d@waferbaby.com>
2023-06-30 17:35:26 +0000

    Switch to the Faraday middleware, clean up profile methods, rename search methods

--- lib/restiny.rb
+++ lib/restiny.rb
@@ -9,6 +9,7 @@ require "restiny/manifest"
 
 require "faraday"
 require "faraday/follow_redirects"
+require "faraday/destiny/api"
 require "securerandom"
 
 module Restiny
@@ -38,48 +39,56 @@ module Restiny
     params = { code: code, grant_type: "authorization_code", client_id: @oauth_client_id }
     params[:redirect_url] = redirect_url unless redirect_url.nil?
 
-    make_api_request(
-      "app/oauth/token/",
-      type: :post,
-      params: params,
-      headers: {
-        "Content-Type" => "application/x-www-form-urlencoded"
-      }
-    )
+    connection.post(
+      "app/oauth/token",
+      params,
+      "Content-Type" => "application/x-www-form-urlencoded"
+    ).body
   end
 
   # Manifest methods
 
+  def get_manifest_url(locale: "en")
+    result = connection.get("Destiny2/Manifest/").body.dig("mobileWorldContentPaths", locale)
+    BUNGIE_URL + result
+  end
+
   def download_manifest(locale: "en")
-    response = get("Destiny2/Manifest/")
+    manifest_url = get_manifest_url
+    raise Restiny::ResponseError.new("Unable to determine manifest URL") if manifest_url.nil?
 
-    manifest_path = response.dig("mobileWorldContentPaths", locale)
-    raise Restiny::ResponseError.new("Unable to determine manifest URL") if manifest_path.nil?
-
-    Manifest.download_by_url(BUNGIE_URL + manifest_path)
+    Manifest.download_by_url(BUNGIE_URL + manifest_url)
   end
 
   # Profile and related methods
 
-  def get_profile(membership_id:, membership_type:, components:)
-    components = prepare_profile_components(components)
+  def get_profile(membership_id:, membership_type:, components:, type_url: nil)
+    if !components.is_a?(Array) || components.empty?
+      raise Restiny::InvalidParamsError.new("Please provide at least one component")
+    end
 
-    get("Destiny2/#{membership_type}/Profile/#{membership_id}/?components=#{components}")
+    url = "Destiny2/#{membership_type}/Profile/#{membership_id}/"
+    url += type_url if type_url
+    url += "?components=#{components.join(",")}"
+
+    connection.get(url).body
   end
 
-  def get_character(character_id:, membership_id:, membership_type:, components:)
-    components = prepare_profile_components(components)
-
-    get(
-      "Destiny2/#{membership_type}/Profile/#{membership_id}/Character/#{character_id}/?components=#{components}"
+  def get_character_profile(character_id:, membership_id:, membership_type:, components:)
+    get_profile(
+      membership_id: membership_id,
+      membership_type: membership_type,
+      components: components,
+      type_url: "Character/#{character_id}"
     )
   end
 
-  def get_instanced_item(item_id:, membership_id:, membership_type:, components:)
-    components = prepare_profile_components(components)
-
-    get(
-      "Destiny2/#{membership_type}/Profile/#{membership_id}/Item/#{item_id}/?components=#{components}"
+  def get_instanced_item_profile(item_id:, membership_id:, membership_type:, components:)
+    get_profile(
+      membership_id: membership_id,
+      membership_type: membership_type,
+      components: components,
+      type_url: "Item/#{item_id}"
     )
   end
 
@@ -88,99 +97,53 @@ module Restiny
   def get_user_memberships_by_id(membership_id, membership_type: Platform::ALL)
     raise Restiny::InvalidParamsError.new("Please provide a membership ID") if membership_id.nil?
 
-    get("User/GetMembershipsById/#{membership_id}/#{membership_type}/")
+    connection.get("User/GetMembershipsById/#{membership_id}/#{membership_type}/").body
   end
 
-  def find_user_by_bungie_name(name, membership_type: Platform::ALL)
+  def search_player_by_bungie_name(name, membership_type: Platform::ALL)
     display_name, display_name_code = name.split("#")
     if display_name.nil? || display_name_code.nil?
       raise Restiny::InvalidParamsError.new("You must provide a valid Bungie name")
     end
 
-    post(
+    connection.post(
       "Destiny2/SearchDestinyPlayerByBungieName/#{membership_type}/",
-      params: {
-        displayName: display_name,
-        displayNameCode: display_name_code
-      }
-    )
+      displayName: display_name,
+      displayNameCode: display_name_code
+    ).body
   end
 
-  def search_users(name:, page: 0)
-    post("User/Search/GlobalName/#{page}", params: { displayNamePrefix: name })
+  def search_users_by_global_name(name:, page: 0)
+    connection.post("User/Search/GlobalName/#{page}", displayNamePrefix: name).body
   end
 
   # General request methods
 
-  def get(url, params: {}, headers: {})
-    make_api_request(url, type: :get, params: params, headers: headers).dig("Response")
-  end
-
-  def post(url, body:, headers: {})
-    make_api_request(url, type: :post, body: body, headers: headers).dig("Response")
-  end
-
   private
-
-  def make_api_request(url, type:, params:, headers: {})
-    raise Restiny::InvalidParamsError.new("You need to set an API key") unless @api_key
-
-    headers[:authorization] = "Bearer #{@oauth_token}" if @oauth_token
-
-    response =
-      case type
-      when :get
-        connection.get(url, params, headers)
-      when :post
-        connection.post(url, params, headers)
-      end
-
-    response.body
-  rescue Faraday::Error => error
-    begin
-      error_body = JSON.parse(error.response_body)
-      status, message = error_body["ErrorStatus"], error_body["Message"]
-    rescue JSON::ParserError
-      status, message = error.response_status, error.message
-    end
-
-    klass =
-      case error
-      when Faraday::ClientError
-        Restiny::RequestError
-      when Faraday::ServerError
-        Restiny::ResponseError
-      else
-        Restiny::Error
-      end
-
-    raise klass.new(message, status)
-  end
 
   def check_oauth_client_id
     raise Restiny::RequestError.new("You need to set an OAuth client ID") unless @oauth_client_id
   end
 
-  def prepare_profile_components(components)
-    if !components.is_a?(Array) || components.empty?
-      raise Restiny::InvalidParamsError.new("Please provide at least one component")
-    end
-
-    components.join(",")
-  end
-
   def default_headers
-    { "User-Agent": "restiny v#{Restiny::VERSION}", "X-API-KEY": @api_key }
+    {
+      "User-Agent": "restiny v#{Restiny::VERSION}",
+      "X-API-KEY": @api_key,
+      "Content-Type": "application/json"
+    }
   end
 
   def connection
-    @connection ||=
-      Faraday.new(url: API_BASE_URL, headers: default_headers) do |faraday|
-        faraday.request :json
-        faraday.request :url_encoded
-        faraday.response :json
-        faraday.response :follow_redirects
-        faraday.response :raise_error
-      end
+    raise Restiny::InvalidParamsError.new("You need to set an API key") unless @api_key
+
+    headers = default_headers
+    headers["authorization"] = "Bearer #{@oauth_token}" if @oauth_token
+
+    Faraday.new(url: API_BASE_URL, headers: headers) do |faraday|
+      faraday.request :url_encoded
+      faraday.request :json
+      faraday.response :follow_redirects
+      faraday.response :destiny_api
+    end
   end
 end
