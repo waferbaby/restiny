6769488ce7b8a2d64c37fc8e785982c0097d6a0a
Daniel Bogan <d@waferbaby.com>
2023-07-02 04:35:44 +0000

    Add the auth middleware

---
+++ lib/faraday/destiny/auth.rb
@@ -0,0 +1,21 @@
+require "faraday"
+require "restiny/errors"
+
+module Faraday
+  module Restiny
+    Faraday::Response.register_middleware(destiny_auth: "Faraday::Restiny::Auth")
+
+    class Auth < Middleware
+      def on_complete(env)
+        return if env["response_body"].empty? || env["url"].to_s !~ /oauth/
+
+        if env["response_body"]["error"]
+          raise ::Restiny::AuthenticationError.new(
+                  env["response_body"]["error_description"],
+                  env["response_body"]["error"]
+                )
+        end
+      end
+    end
+  end
+end
