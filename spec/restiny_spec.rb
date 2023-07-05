31912cd7dc36a1bf0b7168f9a3903eaec8266cef
Daniel Bogan <d@waferbaby.com>
2023-07-05 02:06:58 +0000

    Use the one cassette, since the data is the same

--- spec/restiny_spec.rb
+++ spec/restiny_spec.rb
@@ -1,8 +1,8 @@
 require "spec_helper"
 
 describe Restiny do
-  describe "#get_manifest_url" do
-    context "without a locale", vcr: { cassette_name: "restiny/get_manifest_url" } do
+  describe "#get_manifest_url", vcr: { cassette_name: "restiny/get_manifest_url" } do
+    context "without a locale" do
       it "returns a valid URL for the English manifest" do
         expect(subject.get_manifest_url).to start_with(
           "https://www.bungie.net/common/destiny2_content/sqlite/en/world_sql_content_"
@@ -10,7 +10,7 @@ describe Restiny do
       end
     end
 
-    context "with a given locale", vcr: { cassette_name: "restiny/get_manifest_url_with_locale" } do
+    context "with a given locale" do
       it "returns the correct URL" do
         expect(subject.get_manifest_url(locale: "fr")).to start_with(
           "https://www.bungie.net/common/destiny2_content/sqlite/fr/world_sql_content_"
