c7eb8b88293dc4b7b1249a5886d9ba3005705d8d
Daniel Bogan <d@waferbaby.com>
2023-07-05 02:54:17 +0000

    Switch to match instead of starts_with

--- spec/restiny_spec.rb
+++ spec/restiny_spec.rb
@@ -2,19 +2,25 @@ require "spec_helper"
 
 describe Restiny do
   describe "#get_manifest_url", vcr: { cassette_name: "restiny/get_manifest_url" } do
+    let(:manifest_pattern) do
+      Regexp.new(
+        "https://www.bungie.net/common/destiny2_content/sqlite/#{locale}/world_sql_content_([a-z0-9]+).content"
+      )
+    end
+
     context "without a locale" do
+      let(:locale) { "en" }
+
       it "returns a valid URL for the English manifest" do
-        expect(subject.get_manifest_url).to start_with(
-          "https://www.bungie.net/common/destiny2_content/sqlite/en/world_sql_content_"
-        )
+        expect(subject.get_manifest_url).to match(manifest_pattern)
       end
     end
 
     context "with a given locale" do
+      let(:locale) { "fr" }
+
       it "returns the correct URL" do
-        expect(subject.get_manifest_url(locale: "fr")).to start_with(
-          "https://www.bungie.net/common/destiny2_content/sqlite/fr/world_sql_content_"
-        )
+        expect(subject.get_manifest_url(locale: locale)).to match(manifest_pattern)
       end
     end
   end
