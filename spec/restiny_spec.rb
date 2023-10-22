832b6edd504ef282df7ebf6ce1422b568f9c0160
Daniel Bogan <d@waferbaby.com>
2023-10-22 09:42:45 +0000

    Rework the specs to actually work

--- spec/restiny_spec.rb
+++ spec/restiny_spec.rb
@@ -3,26 +3,43 @@
 require 'spec_helper'
 
 describe Restiny do
-  describe '#get_manifest', vcr: { cassette_name: 'restiny/get_manifest' } do
-    let(:manifest_pattern) do
-      Regexp.new(
-        "https://www.bungie.net/common/destiny2_content/sqlite/#{locale}/world_sql_content_([a-z0-9]+).content"
-      )
+  describe '#download_manifest', vcr: { cassette_name: 'restiny/download_manifest' } do
+    let(:manifest_url) { "https://www.bungie.net/common/destiny2_content/sqlite/#{locale}/world_sql_content_#{manifest_hash}.content" }
+
+    before do
+      zip_path = File.join(__dir__, 'data', 'manifest', locale, 'manifest.zip')
+
+      if File.exist?(zip_path)
+        stub_request(:get, manifest_url).to_return(body: File.read(zip_path), status: 200)
+      end
     end
 
     context 'without a locale' do
-      it 'returns the correct manifest' do
-        manifest = subject.get_manifest
+      let(:manifest_hash) { '82c377013bea4b9c80747756ba4d9726' }
+      let(:locale) { 'en' }
 
-        puts manifest
+      it 'downloads the default English manifest' do
+        subject.download_manifest
+        assert_requested(:get, manifest_url)
       end
     end
 
     context 'with a given locale' do
-      it 'returns the correct manifest' do
-        manifest = subject.get_manifest(locale: 'fr')
+      let(:manifest_hash) { '230929be68cd6ca4dd169ff4d9b07831' }
+      let(:locale) { 'fr' }
 
-        puts manifest
+      it 'download the correct manifest' do
+        subject.download_manifest(locale: 'fr')
+        assert_requested(:get, manifest_url)
+      end
+    end
+
+    context 'with an invalid locale' do
+      let(:manifest_hash) { 'cb9297065f50c42348c9ac04679420e7' }
+      let(:locale) { 'hive' }
+
+      it 'raises an error' do
+        expect { subject.download_manifest(locale: locale) }.to raise_error(Restiny::RequestError, 'Unknown locale')
       end
     end
   end
