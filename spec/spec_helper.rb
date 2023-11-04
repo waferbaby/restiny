298153a7097520be8ecae84725ddcf70bb316f12
Daniel Bogan <d@waferbaby.com>
2023-11-04 07:25:37 +0000

    Default to recording new cassettes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,7 +12,7 @@ Restiny.oauth_client_id = ENV.fetch('DESTINY_OAUTH_CLIENT_ID')
 
 VCR.configure do |c|
   c.cassette_library_dir = File.join(__dir__, 'cassettes')
-  c.default_cassette_options = { serialize_with: :json }
+  c.default_cassette_options = { serialize_with: :json, record: :new_episodes }
 
   c.define_cassette_placeholder('<API_KEY>') { ENV.fetch('DESTINY_API_KEY') }
   c.define_cassette_placeholder('<OAUTH_CLIENT_ID>') { ENV.fetch('DESTINY_OAUTH_CLIENT_ID') }
