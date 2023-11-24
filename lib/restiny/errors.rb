c87660188af091654d02473a2fd2df2c17b2cdc0
Daniel Bogan <d@waferbaby.com>
2023-11-24 21:59:31 +0000

    Simplify the errors

--- lib/restiny/errors.rb
+++ lib/restiny/errors.rb
@@ -1,28 +1,11 @@
 # frozen_string_literal: true
 
 module Restiny
-  class Error < StandardError
-    def initialize(message, status = nil)
-      @status = status
-      super(message)
-    end
-  end
-
-  class NetworkError < Error
-  end
-
-  class RequestError < Error
-  end
-
-  class InvalidParamsError < RequestError
-  end
-
-  class RateLimitedError < RequestError
-  end
-
-  class AuthenticationError < RequestError
-  end
-
-  class ResponseError < Error
-  end
+  class Error < StandardError; end
+  class NetworkError < Error; end
+  class RequestError < Error; end
+  class InvalidParamsError < RequestError; end
+  class RateLimitedError < RequestError; end
+  class AuthenticationError < RequestError; end
+  class ResponseError < Error; end
 end
